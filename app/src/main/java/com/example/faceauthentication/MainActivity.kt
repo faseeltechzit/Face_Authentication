d73cfbb9995c11fe2d272a7421f4861cbf6aa4ea
Faseel - Techzit <faseel.ali@techzit.in>
2024-05-16 11:38:10 +0530

    Removed redundant codes

--- app/src/main/java/com/example/faceauthentication/MainActivity.kt
+++ app/src/main/java/com/example/faceauthentication/MainActivity.kt
@@ -1,22 +1,16 @@
 package com.example.faceauthentication
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
 import androidx.camera.view.LifecycleCameraController
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.faceauthentication.camerax.CameraManager
 import com.example.faceauthentication.databinding.ActivityMainBinding
@@ -26,26 +20,18 @@ import com.us47codex.liveness_detection.LivenessDetector
 import com.us47codex.liveness_detection.tasks.DetectionTask
 import com.us47codex.liveness_detection.tasks.EyesBlinkDetectionTask
 import com.us47codex.liveness_detection.tasks.FacingDetectionTask
-import java.io.ByteArrayOutputStream
-import java.io.File
-
+/**
+ * MainActivity handles the camera and face authentication logic.
+ */
 class MainActivity : AppCompatActivity() {
 
     companion object {
         var applicationContext1: Context? = null
         var name: String = ""
-
-        private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
-        const val FACEDATA = "facedata"
-        const val VERIFIED = "verify"
-        const val EMPLOYEEIMG = "employeeImg"
-        const val CAPTUREIMG = "captureimg"
     }
 
     private var bitmap: Bitmap? = null
     private var result: Float? = null
-    private var selfieImg: String? = null
     private lateinit var cameraManager: CameraManager
     private lateinit var binding: ActivityMainBinding
     private lateinit var cameraController: LifecycleCameraController
@@ -57,6 +43,21 @@ class MainActivity : AppCompatActivity() {
         EyesBlinkDetectionTask()
     )
 
+    // ActivityResultLauncher for requesting permissions
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { granted ->
+        if (granted) {
+            startCamera()
+        } else {
+            Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
+
+    /**
+     * Called when the activity is first created.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         applicationContext1 = this.applicationContext
@@ -65,18 +66,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Request Camera permission and start the camera if granted
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
-            if (granted) {
-                startCamera()
-            } else {
-                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
-                finish()
-            }
-        }.launch(Manifest.permission.CAMERA)
+        // Request Camera permission
+        requestPermissionLauncher.launch(Manifest.permission.CAMERA)
     }
 
-    // Start the camera and set up the camera controller
+    /**
+     * Start the camera and set up the camera controller.
+     */
     private fun startCamera() {
         cameraController = LifecycleCameraController(this)
         cameraController.cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
@@ -97,22 +93,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // Build the Liveness Detector with a listener to handle task events
+    /**
+     * Build the Liveness Detector with a listener to handle task events.
+     * @return An instance of [LivenessDetector]
+     */
     private fun buildLivenessDetector(): LivenessDetector {
         val listener = object : LivenessDetector.Listener {
-            @SuppressLint("SetTextI18n")
             override fun onTaskStarted(task: DetectionTask) {
                 binding.guide.text = task.taskDescription()
             }
 
             override fun onTaskCompleted(task: DetectionTask, isLastTask: Boolean) {
-                takePhoto(
-                    File(cacheDir, "${name}_${task.taskName()}_${System.currentTimeMillis()}.jpg")
-                ) { file ->
-                    imageFiles.add(file.absolutePath)
                     if (isLastTask) {
                         finishForResult()
-                    }
                 }
             }
 
@@ -130,83 +123,32 @@ class MainActivity : AppCompatActivity() {
         return livenessDetector.also { it.setListener(listener) }
     }
 
-    // Handle completion of liveness detection tasks
+    /**
+     * Handle completion of liveness detection tasks.
+     */
     private fun finishForResult() {
         val result = ArrayList(imageFiles.takeLast(livenessDetector.getTaskSize()))
-        setResult(RESULT_OK, Intent().putStringArrayListExtra(ResultContract.RESULT_KEY, result))
+        setResult(RESULT_OK, Intent().putStringArrayListExtra("images", result))
         binding.guide.text = getString(R.string.face_liveliness_check_passed)
         binding.guide2.text = getString(R.string.face_recognition_check_started)
 
         createCameraManager()
-        checkForPermission()
+        cameraManager.startCamera()
         setupUIActions()
     }
 
-    // Capture photo and handle the result
-    private fun takePhoto(file: File, onSaved: (File) -> Unit) {
-        cameraController.takePicture(
-            ImageCapture.OutputFileOptions.Builder(file).build(),
-            ContextCompat.getMainExecutor(this),
-            object : ImageCapture.OnImageSavedCallback {
-                override fun onError(e: ImageCaptureException) {
-                    e.printStackTrace()
-                }
-
-                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                    onSaved(file)
-                }
-            }
-        )
-    }
-
-    // Define contract for result handling between activities
-    class ResultContract : ActivityResultContract<Any?, List<String>?>() {
-
-        companion object {
-            const val RESULT_KEY = "images"
-        }
-
-        override fun createIntent(context: Context, input: Any?): Intent {
-            return Intent(context, MainActivity::class.java)
-        }
-
-        override fun parseResult(resultCode: Int, intent: Intent?): List<String>? {
-            return if (resultCode == RESULT_OK && intent != null) {
-                intent.getStringArrayListExtra(RESULT_KEY)
-            } else null
-        }
-    }
-
-    // Check for necessary permissions and start camera if granted
-    private fun checkForPermission() {
-        if (allPermissionsGranted()) {
-            cameraManager.startCamera()
-        } else {
-            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
-        }
-    }
-
-    // Set up UI actions and event handlers
+    /**
+     * Set up UI actions and event handlers.
+     */
     private fun setupUIActions() {
         binding.cameraSwitch.setOnClickListener {
             cameraManager.changeCameraSelector()
         }
     }
 
-    // Handle the result of permission requests
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                cameraManager.startCamera()
-            } else {
-                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show()
-                finish()
-            }
-        }
-    }
-
-    // Create and configure the CameraManager
+    /**
+     * Create and configure the CameraManager.
+     */
     private fun createCameraManager() {
         cameraManager = CameraManager(
             this,
@@ -218,286 +160,27 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
-    // Handle face detection results
+    /**
+     * Handle face detection results.
+     * @param bitmap The detected face as a bitmap.
+     * @param result The similarity result of the detected face.
+     */
     private fun onFaceDetect(bitmap: Bitmap, result: Float) {
         this.bitmap = bitmap
         this.result = result
         if (result < 1.0f) {
-            Toast.makeText(this, "Verified User", Toast.LENGTH_SHORT).show()
+            binding.guide.text = getString(R.string.face_recognition_successful)
+            binding.guide2.text = getString(R.string.verified_user)
         }
         Log.e("TAG", "onFaceDetect: bitmap : $bitmap result : $result")
     }
 
-    // Process the captured picture (currently empty)
-    private fun processPicture(faceStatus: FaceStatus) {}
-
-    // Check if all required permissions are granted
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    /**
+     * Process the captured picture (currently empty).
+     * @param faceStatus The status of the detected face.
+     */
+    @Suppress("UNUSED_PARAMETER")
+    private fun processPicture(faceStatus: FaceStatus) {
+        // Currently empty. Implement as needed.
     }
 }
-
-
-/*
-package com.example.faceauthentication
-
-import android.Manifest
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContract
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.view.LifecycleCameraController
-import androidx.core.content.ContextCompat
-import com.example.faceauthentication.camerax.CameraManager
-import com.example.faceauthentication.databinding.ActivityMainBinding
-import com.example.faceauthentication.face_recognonize.FaceStatus
-import com.us47codex.liveness_detection.FaceAnalyzer
-import com.us47codex.liveness_detection.LivenessDetector
-import com.us47codex.liveness_detection.tasks.DetectionTask
-import com.us47codex.liveness_detection.tasks.EyesBlinkDetectionTask
-import com.us47codex.liveness_detection.tasks.FacingDetectionTask
-import java.io.File
-
-*/
-/**
- * MainActivity class that handles face authentication using camera, liveness detection,
- * and face recognition. It captures images, analyzes them for liveness, and performs
- * face recognition based on the results.
- *//*
-
-class MainActivity : AppCompatActivity() {
-
-    companion object {
-        private const val REQUEST_CODE_PERMISSIONS = 10
-    }
-
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var cameraController: LifecycleCameraController
-    private lateinit var cameraManager: CameraManager
-
-    private var bitmap: Bitmap? = null
-    private var result: Float? = null
-    private val imageFiles = arrayListOf<String>()
-
-    // Initialize LivelinessDetector with necessary detection tasks
-    private val livenessDetector = LivenessDetector(FacingDetectionTask(), EyesBlinkDetectionTask())
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        // Request camera permission
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
-            if (granted) setupCamera() else {
-                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
-                finish()
-            }
-        }.launch(Manifest.permission.CAMERA)
-
-        binding.cameraSwitch.setOnClickListener{
-            cameraManager.changeCameraSelector()
-        }
-    }
-
-    */
-/**
- * Sets up the camera controller and binds it to the lifecycle of this activity.
- * Also configures the camera switch button to toggle between front and back cameras.
- *//*
-
-    private fun setupCamera() {
-        cameraController = LifecycleCameraController(this).apply {
-            cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
-            setImageAnalysisAnalyzer(
-                ContextCompat.getMainExecutor(this@MainActivity),
-                FaceAnalyzer(buildLivelinessDetector())
-            )
-            bindToLifecycle(this@MainActivity)
-        }
-        binding.cameraPreview.controller = cameraController
-
-        binding.cameraSwitch.setOnClickListener {
-            cameraController.cameraSelector =
-                if (cameraController.cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) CameraSelector.DEFAULT_FRONT_CAMERA else CameraSelector.DEFAULT_BACK_CAMERA
-        }
-    }
-
-    */
-/**
- * Builds and configures the LivelinessDetector with a listener for task events.
- * This includes handling task started, completed, and failed events.
- *//*
-
-    private fun buildLivelinessDetector(): LivenessDetector {
-        val listener = object : LivenessDetector.Listener {
-            override fun onTaskStarted(task: DetectionTask) {
-                binding.guide.text = task.taskDescription()
-            }
-
-            override fun onTaskCompleted(task: DetectionTask, isLastTask: Boolean) {
-                takePhoto(File(cacheDir, "${System.currentTimeMillis()}_${task.taskName()}.jpg")) {
-                    synchronized(imageFiles) {
-                        imageFiles.add(it.absolutePath)
-                    }
-                    if (isLastTask) finishForResult()
-                }
-            }
-
-            override fun onTaskFailed(task: DetectionTask, code: Int) {
-                val message = when (code) {
-                    LivenessDetector.ERROR_MULTI_FACES -> "Please make sure there is only one face on the screen."
-                    LivenessDetector.ERROR_NO_FACE -> "Please make sure there is a face on the screen."
-                    LivenessDetector.ERROR_OUT_OF_DETECTION_RECT -> "Please make sure there is a face in the Rectangle."
-                    else -> "${task.taskName()} Failed."
-                }
-                binding.guide2.text = message
-            }
-        }
-        return livenessDetector.apply { setListener(listener) }
-    }
-
-    */
-/**
- * Completes the liveness detection process, collects the captured images,
- * and prepares the result to be sent back to the calling activity.
- *//*
-
-    private fun finishForResult() {
-        val resultImages =
-            synchronized(imageFiles) { ArrayList(imageFiles.takeLast(livenessDetector.getTaskSize())) }
-        setResult(
-            RESULT_OK,
-            Intent().putStringArrayListExtra(ResultContract.RESULT_KEY, resultImages)
-        )
-        binding.guide.text = getString(R.string.face_liveliness_check_passed)
-        binding.guide2.text = getString(R.string.face_recognition_check_started)
-        setupCameraManager()
-    }
-
-    */
-/**
- * Captures a photo and saves it to the specified file.
- *
- * @param file The file to save the captured image to.
- * @param onSaved Callback invoked when the image is successfully saved.
- *//*
-
-    private fun takePhoto(file: File, onSaved: (File) -> Unit) {
-        cameraController.takePicture(
-            ImageCapture.OutputFileOptions.Builder(file).build(),
-            ContextCompat.getMainExecutor(this),
-            object : ImageCapture.OnImageSavedCallback {
-                override fun onError(e: ImageCaptureException) {
-                    e.printStackTrace()
-                }
-
-                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                    onSaved(file)
-                }
-            }
-        )
-    }
-
-    */
-/**
- * Custom ActivityResultContract to handle results from MainActivity.
- *//*
-
-    class ResultContract : ActivityResultContract<Any?, List<String>?>() {
-        companion object {
-            const val RESULT_KEY = "images"
-        }
-
-        override fun createIntent(context: Context, input: Any?) =
-            Intent(context, MainActivity::class.java)
-
-        override fun parseResult(resultCode: Int, intent: Intent?) =
-            if (resultCode == RESULT_OK) intent?.getStringArrayListExtra(RESULT_KEY) else null
-    }
-
-    */
-/**
- * Sets up the CameraManager which handles camera operations, preview, and graphic overlays.
- *//*
-
-    private fun setupCameraManager() {
-        cameraManager = CameraManager(
-            this,
-            binding.cameraPreview,
-            this,
-            binding.graphicOverlayFinder,
-            ::processPicture,
-            ::onFaceDetect
-        )
-    }
-
-    */
-/**
- * Handles face detection results, updates the bitmap and result variables,
- * and displays a toast if the user is verified.
- *
- * @param bitmap The detected face bitmap.
- * @param result The confidence score of the detected face.
- *//*
-
-    private fun onFaceDetect(bitmap: Bitmap, result: Float) {
-        this.bitmap = bitmap
-        this.result = result
-        if (result < 1.0f) Toast.makeText(this, "Verified User", Toast.LENGTH_SHORT).show()
-        Log.e("TAG", "onFaceDetect: bitmap : $bitmap result : $result")
-    }
-
-    */
-/**
- * Processes the picture for face status. To be implemented as needed.
- *
- * @param faceStatus The face status object.
- *//*
-
-    private fun processPicture(faceStatus: FaceStatus) {}
-
-    */
-/**
- * Checks if all required permissions are granted.
- *
- * @return True if all permissions are granted, false otherwise.
- *//*
-
-    private fun allPermissionsGranted() = arrayOf(Manifest.permission.CAMERA).all {
-        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
-    }
-
-    */
-/**
- * Handles the result of permission requests.
- *
- * @param requestCode The request code passed in requestPermissions.
- * @param permissions The requested permissions.
- * @param grantResults The grant results for the corresponding permissions.
- *//*
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE_PERMISSIONS && allPermissionsGranted()) {
-            setupCamera()
-        } else {
-            Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show()
-            finish()
-        }
-    }
-}
-*/
